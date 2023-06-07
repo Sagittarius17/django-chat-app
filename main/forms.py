db88c0778d8cd04ff641a1e9e505d79f00a66a0f
Shuvendu Singha <108450702+Sagittarius17@users.noreply.github.com>
2023-06-07 23:15:52 +0530

    Update forms.py

--- main/forms.py
+++ main/forms.py
@@ -5,9 +5,9 @@ from django.contrib.auth.models import User
 class SignupForm(UserCreationForm):
     email = forms.EmailField()
 
-    class Meta:
-        model = User
-        fields = ('username', 'email', 'password1', 'password2')
+#     class Meta:
+#         model = User
+#         fields = ('username', 'email', 'password1', 'password2')
 
 class LoginForm(forms.Form):
     username = forms.CharField()
