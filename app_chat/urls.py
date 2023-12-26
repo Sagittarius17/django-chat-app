0d119995fdaf0a6f3502b06bd1bd9af53b3d162c
shuvendu <108450702+Sagittarius17@users.noreply.github.com>
2023-12-26 22:29:36 +0530

    signup

--- app_chat/urls.py
+++ app_chat/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index_view, name='index_view')
+    path('', views.index_view, name='index_view'),
+    path('signup/', views.signup, name='signup')
 ]
--- app_chat/views.py
+++ app_chat/views.py
@@ -1,5 +1,22 @@
-from django.shortcuts import render
+from django.contrib.auth import login
+from django.shortcuts import render, redirect
 
-# Create your views here.
+from .forms import *
+
 def index_view(request):
-    return render(request, 'app_chat/base.html')
+    return render(request, 'base.html')
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        
+        if form.is_valid():
+            user = form.save()
+            
+            login(request, user)
+            
+            return redirect('welcome')
+    else:
+        form = SignUpForm()
+    
+    return render(request, 'app_chat/signup.html', {'form': form})
