ea63114ad2e017fc3449b25bf69563627c8cefab
shuvendu <108450702+Sagittarius17@users.noreply.github.com>
2023-12-29 14:49:35 +0530

    working good till now...

--- app_room/views.py
+++ app_room/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from django.contrib import messages as msg
 from .models import *
 
 @login_required
@@ -10,8 +10,25 @@ def rooms(request):
     return render(request, 'app_room/rooms.html', {'rooms': rooms})
 
 @login_required
-def room(request, slug):
-    room = Room.objects.get(slug=slug)
-    messages = Message.objects.filter(room=room)[0:25]
+# def room(request, slug):
+#     room = Room.objects.get(slug=slug)
+#     messages = Message.objects.filter(room=room)[0:25]
     
-    return render(request, 'app_room/room.html', {'room': room}, {'messages': messages})
+#     return render(request, 'app_room/room.html', {'room': room}, {'messages': messages})
+
+def room(request, slug):
+    try:
+        room = get_object_or_404(Room, slug=slug)
+        messages = Message.objects.filter(room=room)[0:25]
+        
+        return render(request, 'app_room/room.html', {'room': room, 'messages': messages})
+    
+    except Room.DoesNotExist:
+        msg.error(request, 'Room not found.')
+        return render(request, 'app_room/room_not_found.html', status=404)
+
+    except Exception as e:
+        # Log the exception for debugging purposes
+        print(f"An error occurred: {e}")
+        msg.error(request, 'An error occurred.')
+        return render(request, 'app_room/room_error.html', status=500)
